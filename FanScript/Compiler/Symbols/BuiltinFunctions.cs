b3f332db0ae2e61a2b2c6a4fa1240ae40b2259b6
BitcoderCZ <82174496+BitcoderCZ@users.noreply.github.com>
2024-10-14 11:17:03 +0200

    Added docs for Functions/Physics

--- FanScript/Compiler/Symbols/BuiltinFunctions.cs
+++ FanScript/Compiler/Symbols/BuiltinFunctions.cs
@@ -845,10 +845,37 @@ namespace FanScript.Compiler.Symbols
 
         private static class Physics
         {
+            [FunctionDoc(
+                Info = """
+                Adds <link type="param">force</> and/or <link type="param">torque</> to <link type="param">object</>.
+                """,
+                ParameterInfos = [
+                    """
+                    The object that the force will be applied to.
+                    """,
+                    """
+                    The force to apply to <link type="param">object</>.
+                    """,
+                    """
+                    Where on <link type="param">object</> should <link type="param">force</> be applied at (center of mass by default).
+                    """,
+                    """
+                    The rotational force to apply to <link type="param">object</>.
+                    """
+                ],
+                Related = [
+                    """
+                    <link type="func">setVelocity;obj;vec3;vec3</>
+                    """,
+                    """
+                    <link type="func">getVelocity;obj;vec3;vec3</>
+                    """
+                ]
+            )]
             public static readonly FunctionSymbol AddForce
                 = new BuiltinFunctionSymbol("addForce",
                 [
-                   new ParameterSymbol("object", TypeSymbol.Object),
+                    new ParameterSymbol("object", TypeSymbol.Object),
                     new ParameterSymbol("force", TypeSymbol.Vector3),
                     new ParameterSymbol("applyAt", TypeSymbol.Vector3),
                     new ParameterSymbol("torque", TypeSymbol.Vector3),
@@ -856,72 +883,225 @@ namespace FanScript.Compiler.Symbols
                 {
                     IsMethod = true,
                 };
+
+            [FunctionDoc(
+                Info = """
+                Gets the <link type="param">object</>'s velocity.
+                """,
+                ParameterInfos = [
+                    null,
+                    """
+                    Linear velocity of <link type="param">object</> (units/second).
+                    """,
+                    """
+                    Angular velocity of <link type="param">object</> (degrees/second).
+                    """
+                ],
+                Related = [
+                    """
+                    <link type="func">setVelocity;obj;vec3;vec3</>
+                    """,
+                    """
+                    <link type="func">addForce;obj;vec3;vec3;vec3</>
+                    """
+                ]
+            )]
             public static readonly FunctionSymbol GetVelocity
                 = new BuiltinFunctionSymbol("getVelocity",
                 [
-                   new ParameterSymbol("object", TypeSymbol.Object),
+                    new ParameterSymbol("object", TypeSymbol.Object),
                     new ParameterSymbol("velocity", Modifiers.Out, TypeSymbol.Vector3),
                     new ParameterSymbol("spin", Modifiers.Out, TypeSymbol.Vector3),
                 ], TypeSymbol.Void, (call, context) => emitXX(call, context, 2, Blocks.Physics.GetVelocity))
                 {
                     IsMethod = true,
                 };
+
+            [FunctionDoc(
+                Info = """
+                Sets the linear and angular velocity of <link type="param">object</>.
+                """,
+                ParameterInfos = [
+                    null,
+                    """
+                    The linear velocity (units/second).
+                    """,
+                    """
+                    The angular velocity (degrees/second).
+                    """
+                ],
+                Related = [
+                    """
+                    <link type="func">getVelocity;obj;vec3;vec3</>
+                    """,
+                    """
+                    <link type="func">addForce;obj;vec3;vec3;vec3</>
+                    """
+                ]
+            )]
             public static readonly FunctionSymbol SetVelocity
                 = new BuiltinFunctionSymbol("setVelocity",
                 [
-                   new ParameterSymbol("object", TypeSymbol.Object),
+                    new ParameterSymbol("object", TypeSymbol.Object),
                     new ParameterSymbol("velocity", TypeSymbol.Vector3),
                     new ParameterSymbol("spin", TypeSymbol.Vector3),
                 ], TypeSymbol.Void, (call, context) => emitAX0(call, context, Blocks.Physics.SetVelocity))
                 {
                     IsMethod = true,
                 };
+
+            [FunctionDoc(
+                Info = """
+                Restricts <link type="param">object</>'s movement, forces applied to <link type="param">object</> are multiplied by <link type="param">position</> and <link type="param">rotation</>.
+                """,
+                ParameterInfos = [
+                    null,
+                    """
+                    The movement multiplier.
+                    """,
+                    """
+                    The rotation multiplier.
+                    """
+                ],
+                Remarks = [
+                    """
+                    Negative numbers reverse physics and numbers bigger than 1 increase them.
+                    """
+                ]
+            )]
             public static readonly FunctionSymbol SetLocked
                 = new BuiltinFunctionSymbol("setLocked",
                 [
-                   new ParameterSymbol("object", TypeSymbol.Object),
+                    new ParameterSymbol("object", TypeSymbol.Object),
                     new ParameterSymbol("position", TypeSymbol.Vector3),
                     new ParameterSymbol("rotation", TypeSymbol.Vector3),
                 ], TypeSymbol.Void, (call, context) => emitAX0(call, context, Blocks.Physics.SetLocked))
                 {
                     IsMethod = true,
                 };
+
+            [FunctionDoc(
+                Info = """
+                Sets the mass of <link type="param">object</>.
+                """,
+                ParameterInfos = [
+                    null,
+                    """
+                    The new mass of <link type="param">object</> (the default values is determined by the volume of <link type="param">object</>'s collider).
+                    """
+                ]
+            )]
             public static readonly FunctionSymbol SetMass
                 = new BuiltinFunctionSymbol("setMass",
                 [
-                   new ParameterSymbol("object", TypeSymbol.Object),
+                    new ParameterSymbol("object", TypeSymbol.Object),
                     new ParameterSymbol("mass", TypeSymbol.Float),
                 ], TypeSymbol.Void, (call, context) => emitAX0(call, context, Blocks.Physics.SetMass))
                 {
                     IsMethod = true,
                 };
+
+            [FunctionDoc(
+                Info = """
+                Sets the friction of <link type="param">object</>.
+                """,
+                ParameterInfos = [
+                    null,
+                    """
+                    How much friction to apply to <link type="param">object</> when colliding with other objects (0.5 by default).
+                    """
+                ]
+            )]
             public static readonly FunctionSymbol SetFriction
                 = new BuiltinFunctionSymbol("setFriction",
                 [
-                   new ParameterSymbol("object", TypeSymbol.Object),
+                    new ParameterSymbol("object", TypeSymbol.Object),
                     new ParameterSymbol("friction", TypeSymbol.Float),
                 ], TypeSymbol.Void, (call, context) => emitAX0(call, context, Blocks.Physics.SetFriction))
                 {
                     IsMethod = true,
                 };
+
+            [FunctionDoc(
+                Info = """
+                Sets how bouncy <link type="param">object</> is (how much momentum it retains after each bounce).
+                """,
+                ParameterInfos = [
+                    null,
+                    """
+                    How much momentum <link type="param">object</> retains after each bounce (0 - 1, if higher, <link type="param">object</>'s velocity will increase after each jump).
+                    """
+                ]
+            )]
             public static readonly FunctionSymbol SetBounciness
                 = new BuiltinFunctionSymbol("setBounciness",
                 [
-                   new ParameterSymbol("object", TypeSymbol.Object),
+                    new ParameterSymbol("object", TypeSymbol.Object),
                     new ParameterSymbol("bounciness", TypeSymbol.Float),
                 ], TypeSymbol.Void, (call, context) => emitAX0(call, context, Blocks.Physics.SetBounciness))
                 {
                     IsMethod = true,
                 };
+
+            [FunctionDoc(
+                Info = """
+                Sets the direction (and magnitude) in which any physics objects falls. 
+                """,
+                ParameterInfos = [
+                    """
+                    Default is (0, -9.8, 0).
+                    """
+                ]
+            )]
             public static readonly FunctionSymbol SetGravity
                 = new BuiltinFunctionSymbol("setGravity",
                 [
-                   new ParameterSymbol("gravity", TypeSymbol.Vector3),
+                    new ParameterSymbol("gravity", TypeSymbol.Vector3),
                 ], TypeSymbol.Void, (call, context) => emitAX0(call, context, Blocks.Physics.SetGravity));
+
+            [FunctionDoc(
+                Info = """
+                Creates a <link type="param">constraint</> (an invisible connection (rod) between 2 objects) between <link type="param">part</> and <link type="param">base</>.
+                """,
+                ParameterInfos = [
+                    """
+                    The object that will be glued on.
+                    """,
+                    """
+                    The object that will be glued on the <link type="param">base</>.
+                    """,
+                    """
+                    The other end of the constraint rod.
+                    """,
+                    """
+                    The created constraint.
+                    """
+                ],
+                Related = [
+                    """
+                    <link type="func">linearLimits;constr;vec3;vec3</>
+                    """,
+                    """
+                    <link type="func">angularLimits;constr;vec3;vec3</>
+                    """,
+                    """
+                    <link type="func">linearSpring;constr;vec3;vec3</>
+                    """,
+                    """
+                    <link type="func">angularSpring;constr;vec3;vec3</>
+                    """,
+                    """
+                    <link type="func">linearMotor;constr;vec3;vec3</>
+                    """,
+                    """
+                    <link type="func">angularMotor;constr;vec3;vec3</>
+                    """
+                ]
+            )]
             public static readonly FunctionSymbol AddConstraint
                 = new BuiltinFunctionSymbol("addConstraint",
                 [
-                   new ParameterSymbol("base", TypeSymbol.Object),
+                    new ParameterSymbol("base", TypeSymbol.Object),
                     new ParameterSymbol("part", TypeSymbol.Object),
                     new ParameterSymbol("pivot", TypeSymbol.Vector3),
                     new ParameterSymbol("constraint", Modifiers.Out, TypeSymbol.Constraint),
@@ -929,60 +1109,180 @@ namespace FanScript.Compiler.Symbols
                 {
                     IsMethod = true,
                 };
+
+            [FunctionDoc(
+                Info = """
+                Sets the linear limits of <link type="param">constraint</>.
+                """,
+                ParameterInfos = [
+                    null,
+                    """
+                    The lower limit.
+                    """,
+                    """
+                    The upper limit.
+                    """
+                ],
+                Related = [
+                    """
+                    <link type="func">addConstraint;obj;obj;vec3;constr</>
+                    """
+                ]
+            )]
             public static readonly FunctionSymbol LinearLimits
                 = new BuiltinFunctionSymbol("linearLimits",
                 [
-                   new ParameterSymbol("constraint", TypeSymbol.Constraint),
+                    new ParameterSymbol("constraint", TypeSymbol.Constraint),
                     new ParameterSymbol("lower", TypeSymbol.Vector3),
                     new ParameterSymbol("upper", TypeSymbol.Vector3),
                 ], TypeSymbol.Void, (call, context) => emitAX0(call, context, Blocks.Physics.LinearLimits))
                 {
                     IsMethod = true,
                 };
+
+            [FunctionDoc(
+                Info = """
+                Sets the angular limits of <link type="param">constraint</>.
+                """,
+                ParameterInfos = [
+                    null,
+                    """
+                    The lower angular limit (in degrees).
+                    """,
+                    """
+                    The upper angular limit (in degrees).
+                    """
+                ],
+                Related = [
+                    """
+                    <link type="func">addConstraint;obj;obj;vec3;constr</>
+                    """
+                ]
+            )]
             public static readonly FunctionSymbol AngularLimits
                 = new BuiltinFunctionSymbol("angularLimits",
                 [
-                   new ParameterSymbol("constraint", TypeSymbol.Constraint),
+                    new ParameterSymbol("constraint", TypeSymbol.Constraint),
                     new ParameterSymbol("lower", TypeSymbol.Vector3),
                     new ParameterSymbol("upper", TypeSymbol.Vector3),
                 ], TypeSymbol.Void, (call, context) => emitAX0(call, context, Blocks.Physics.AngularLimits))
                 {
                     IsMethod = true,
                 };
+
+            [FunctionDoc(
+                Info = """
+                Makes the constraint springy, <link type="func">linearLimits;constr;vec3;vec3</> must be called before for linear spring to work.
+                """,
+                ParameterInfos = [
+                    null,
+                    """
+                    How stiff the sping will be.
+                    """,
+                    """
+                    How much damping (drag) to apply.
+                    """
+                ],
+                Related = [
+                    """
+                    <link type="func">addConstraint;obj;obj;vec3;constr</>
+                    """
+                ]
+            )]
             public static readonly FunctionSymbol LinearSpring
                 = new BuiltinFunctionSymbol("linearSpring",
                 [
-                   new ParameterSymbol("constraint", TypeSymbol.Constraint),
+                    new ParameterSymbol("constraint", TypeSymbol.Constraint),
                     new ParameterSymbol("stiffness", TypeSymbol.Vector3),
                     new ParameterSymbol("damping", TypeSymbol.Vector3),
                 ], TypeSymbol.Void, (call, context) => emitAX0(call, context, Blocks.Physics.LinearSpring))
                 {
                     IsMethod = true,
                 };
+
+            [FunctionDoc(
+                Info = """
+                Makes the constraint springy, <link type="func">angularLimits;constr;vec3;vec3</> must be called before for angular spring to work.
+                """,
+                ParameterInfos = [
+                    null,
+                    """
+                    How stiff the sping will be.
+                    """,
+                    """
+                    How much damping (drag) to apply.
+                    """
+                ],
+                Related = [
+                    """
+                    <link type="func">addConstraint;obj;obj;vec3;constr</>
+                    """
+                ]
+            )]
             public static readonly FunctionSymbol AngularSpring
                 = new BuiltinFunctionSymbol("angularSpring",
                 [
-                   new ParameterSymbol("constraint", TypeSymbol.Constraint),
+                    new ParameterSymbol("constraint", TypeSymbol.Constraint),
                     new ParameterSymbol("stiffness", TypeSymbol.Vector3),
                     new ParameterSymbol("damping", TypeSymbol.Vector3),
                 ], TypeSymbol.Void, (call, context) => emitAX0(call, context, Blocks.Physics.AngularSpring))
                 {
                     IsMethod = true,
                 };
+
+            [FunctionDoc(
+                Info = """
+                Makes <link type="param">constraint</> move.
+                """,
+                ParameterInfos = [
+                    null,
+                    """
+                    The speed at which to move at.
+                    """,
+                    """
+                    How much force to apply.
+                    """
+                ],
+                Related = [
+                    """
+                    <link type="func">addConstraint;obj;obj;vec3;constr</>
+                    """
+                ]
+            )]
             public static readonly FunctionSymbol LinearMotor
                 = new BuiltinFunctionSymbol("linearMotor",
                 [
-                   new ParameterSymbol("constraint", TypeSymbol.Constraint),
+                    new ParameterSymbol("constraint", TypeSymbol.Constraint),
                     new ParameterSymbol("speed", TypeSymbol.Vector3),
                     new ParameterSymbol("force", TypeSymbol.Vector3),
                 ], TypeSymbol.Void, (call, context) => emitAX0(call, context, Blocks.Physics.LinearMotor))
                 {
                     IsMethod = true,
                 };
+
+            [FunctionDoc(
+                Info = """
+                Makes <link type="param">constraint</> rotate.
+                """,
+                ParameterInfos = [
+                    null,
+                    """
+                    The speed at which to rotate at.
+                    """,
+                    """
+                    How much force to apply.
+                    """
+                ],
+                Related = [
+                    """
+                    <link type="func">addConstraint;obj;obj;vec3;constr</>
+                    """
+                ]
+            )]
             public static readonly FunctionSymbol AngularMotor
                 = new BuiltinFunctionSymbol("angularMotor",
                 [
-                   new ParameterSymbol("constraint", TypeSymbol.Constraint),
+                    new ParameterSymbol("constraint", TypeSymbol.Constraint),
                     new ParameterSymbol("speed", TypeSymbol.Vector3),
                     new ParameterSymbol("force", TypeSymbol.Vector3),
                 ], TypeSymbol.Void, (call, context) => emitAX0(call, context, Blocks.Physics.AngularMotor))
